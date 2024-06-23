012ec820ac65f0914955b07696931ce34fb75494
DoodieBot <140124349+DoodieBot@users.noreply.github.com>
2024-06-23 23:55:33 +0000

    ♠

--- bank.py
+++ bank.py
@@ -8,16 +8,27 @@ from account import Personal_Account
 
 class BankManager:
     def __init__(self):
-        self.account=[]
+        self._accounts=[]
      
     def existing_account(self, acc_no):
-        for account in self._account:
+        for account in self._accounts:
             if account.get_acc_no() == acc_no:
                 return account
         return False
     
-    def add_account(Self, new_account):
-        if new_account == Self.existing_account:
-            raise ValueError("no bueno")
-        else:
-            Self.account.append(new_account) 
+    def add_account(self, new_account_no, new_account_name):
+        new_account = self.existing_account(new_account_no)
+        if new_account:
+            raise ValueError("account already exists")
+        else:            
+            self._accounts.append(Personal_Account(new_account_name, new_account_no, 0))
+            print("account sucsefully created")
+        
+        
+        
+        
+        
+        
+man1 = BankManager()
+
+man1.add_account("david","1234")
--- main.py
+++ main.py
@@ -4,3 +4,10 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+from bank import BankManager
+from account import Personal_Account
+  
+
+def add_acc_ui(bank):
+    name = input("Enter your Name: ")
+    acc_no = input("Enter the randomised 4 digit bank identifacation number:")
